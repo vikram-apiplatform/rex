724bad16f104b65a72bbbcfaa485ff3aa02e790d
vikram-apiplatform <vikramrko786@gmail.com>
2022-07-28 09:11:21 +0530

    removed all the changes

--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -8,39 +8,65 @@ import {map} from 'rxjs/operators';
 })
 export class ServicesService {
 
-  httpOptions: any = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'pkey': '3fbbb8bc5a969f503fdb66e7d90509d6',
-      'apikey': '7xR2sYhqRAdfhhyu6jMo9E9hi4fRazuw'
-    })
-  };
+  httpOptions: any;
 
   constructor(private http: HttpClient) {
-
   }
 
-  getListing() {
+  getToken() {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'pkey': '3fbbb8bc5a969f503fdb66e7d90509d6',
+        'apikey': '7xR2sYhqRAdfhhyu6jMo9E9hi4fRazuw'
+      })
+    };
+    const url = 'https://dev-vikram.gateway.apiplatform.io/v1/rexLogin';
 
-    const url = 'https://dev-vikram.gateway.apiplatform.io/v1/rexListing';
+    const payload = {
+      "email": "mick@foxs.com.au",
+      "password": "Georgia1"
+    }
 
-    return this.http.get(url, this.httpOptions)
+    return this.http.post(url, payload, this.httpOptions)
       .pipe(map(res => res));
   }
 
-  getPublishedListing() {
+  getListings(token: any) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    const url = 'https://api.rexsoftware.com/v1/rex/listings/search';
 
-    const url = 'https://dev-vikram.gateway.apiplatform.io/v1/rexPublishedListings';
+    const payload = {
+      "limit": 100,
+      "offset": 0,
+      "criteria": [
+        {"system_listing_state": "current"}
+      ]
+    }
 
-    return this.http.get(url, this.httpOptions)
+    return this.http.post(url, payload, this.httpOptions)
       .pipe(map(res => res));
   }
 
-  getProperty(id: any) {
+  getProperty(token: any, id: any) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    const url = 'https://api.rexsoftware.com/v1/rex/properties/read';
 
-    const url = 'https://dev-vikram.gateway.apiplatform.io/v1/rexProperty?id=' + id;
+    const payload = {
+      "id": id
+    }
 
-    return this.http.get(url, this.httpOptions)
+    return this.http.post(url, payload, this.httpOptions)
       .pipe(map(res => res));
   }
 }
